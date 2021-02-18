714e81f2a48f13ca960b45ad6ba5bf73b1c2bba2
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-02-19 01:31:27 +0900

    算術演算子 終了

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 
 struct State<'a> {
     stream: &'a str, // Clion teach me lifetime.
@@ -15,10 +16,11 @@ fn main() {
         pointer: 0,
     };
 
-    println!("{}", eval(&mut state).unwrap());
+    println!("{:?}", eval(&mut state).unwrap());
 }
 
 fn eval(state: &mut State) -> Option<usize> {
+
     let c = state.stream
         .chars()
         .nth(state.pointer)?;
@@ -26,13 +28,35 @@ fn eval(state: &mut State) -> Option<usize> {
     state.pointer += 1;
 
     match c {
+        _ if c.is_whitespace() => {
+            return eval(state);
+        }
         '0'..='9' => {
-            let num = c.to_digit(10)?;
+            let mut num = c.to_digit(10)?;
+            while state.pointer <= state.stream.len() - 1 && state.stream.chars().nth(state.pointer)?.is_digit(10) { // ここではis_digitでbool返す
+                num = num * 10 + state.stream.chars().nth(state.pointer)?.to_digit(10)?;
+                state.pointer += 1;
+            }
             return Some(num as usize); // require explicitly convert
         }
-        _ => {
-            return None;
+        '+' | '-' | '*' | '/' => {
+            let num_1 = eval(state)?;
+            let num_2 = eval(state)?;
+            let result = match c {
+                '+' => num_1 + num_2,
+                '-' => num_1 - num_2,
+                '*' => num_1 * num_2,
+                '/' => num_1 / num_2,
+                _ => error(format!("Invalid operator: {:?}", c)),
+            };
+            return Some(result);
         }
+        _ => error(format!("Invalid character: {:?}, {:?}", c, state.pointer)),
     }
+}
+
+fn error(err_script: String) -> ! {
+    eprintln!("{}", err_script);
+    process::exit(1);
 }
 
