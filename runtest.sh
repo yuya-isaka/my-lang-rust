a61df88dc3fec64d259d920e64ae8c81fff7de00
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-02-19 01:31:07 +0900

    変数の書き方注意よ

--- runtest.sh
+++ runtest.sh
@@ -5,7 +5,8 @@ mylang="./target/debug/my-lang-rust"
 runtest() {
   input="$1"
   expected="$2"
-  output=$(RUST_BACKTRACE=1 $mylang $input)
+  output=$(RUST_BACKTRACE=1 "$mylang" "$input") # ここのミスで3時間くらい潰した．くそっっっっっ．
+  # $で囲った中でも，変数使う時はダブルクオートいるんだよ
 
   # error
   if [ "$expected" != "$output" ]; then
@@ -26,5 +27,16 @@ cargo build
 
 runtest "1" "1"
 runtest "5" 5
+runtest " 5" 5
+runtest "33" 33
+runtest " 33" 33
+runtest "    33    " 33
+
+
+runtest '+ 1 2' '3'
+runtest '+ 10 20' '30'
+runtest '- 10 1' '9'
+runtest '* 2 3' '6'
+runtest '/ 10 5' '2'
 
 echo OK
